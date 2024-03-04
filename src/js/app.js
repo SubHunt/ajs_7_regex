18c8047f1bc3e74c4eb0a0a226eae14607c5c624
Sergey Matofonov <phaeton.kz@gmail.com>
2024-03-04 22:05:27 +0300

    change return

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,6 @@ export default class Validator {
       const name = this.nickname;
       const check1 = /^[^\d_-]+[\dA-Za-z_-]*[^\d_-]+$/.test(name);
       const check2 = !(/\d{4}/.test(name));
-      if (check1 && check2) {
-        return true;
-      }
-      return false;
+      return check1 && check2;
     }
   }
